7a69b68176f45112dfc6157c2e9d8ecf262e4a46
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-06-13 15:01:48 +0200

    feat: move method

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,27 @@ type rover = {
 
 const roverEmitter = new EventEmitter();
 
-roverEmitter.on("move", ({ direction, steps, rover }) => {
+roverEmitter.on("move", ({ direction, steps, rover }) => move(direction, steps, rover));
+
+/**
+ * set the move instruction
+ */
+const move = (
+  direction: cardinalPoint,
+  steps: number,
+  rover: rover
+): {
+  x: number;
+  y: number;
+} => {
   const newOrderedPair = stepsToCardinalPoints(direction, steps);
   const temp = structuredClone(rover);
   temp.x += newOrderedPair.x;
   temp.y += newOrderedPair.y;
   console.log(`previus position`, rover);
   console.log(`actual position`, temp);
-});
+  return temp;
+};
 
 /**
  * Command Line Interface
@@ -38,15 +51,15 @@ const cli = () => {
   rl.prompt();
 
   rl.on("line", (input: string) => {
-   
-    const temp = input.split(' ')
-	const command = temp[0] ?? ''
-    if (command === "move"){
-        const direction = temp[1] ?? ''
-        const steps = temp[2]? Number(temp[2]) : 0
-        roverEmitter.emit("move", { direction, steps, rover });
+    const temp = input.split(" ");
+    const command = temp[0] ?? "";
+    if (command === "move") {
+      const direction = temp[1] ?? "";
+      const steps = temp[2] ? Number(temp[2]) : 0;
+      const x = roverEmitter.emit("move", { direction, steps, rover });
+      console.log(x);
     }
-      
+
     rl.prompt();
   });
 };
