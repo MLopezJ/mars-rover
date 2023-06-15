feb2fe27ecc15f9eb5f2454fc9969a35896262b4
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-15 13:16:43 +0200

    feat: describe implementation process

---
+++ src/step2.ts
@@ -0,0 +1,15 @@
+/**
+ * Step 2: implementation phase
+ *
+ * Once the goal is clear, the phase where code should be created in order to make functionalities work start.
+ *
+ * This phase MUST not start if all requirements are not described in the API description (last step).
+ */
+
+import { MarsRover } from "./marsRover";
+
+const rover = new MarsRover({ x: 0, y: 0, dir: "N" });
+console.log(rover.move("S", 8));
+rover.on("moved", (newPosition: { x: number; y: number }) => {
+  console.log(`New position is`, newPosition.x, newPosition.y); // x: 0, y: 8
+});
